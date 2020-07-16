90530b00fe777b4629328f6f62fe41d24467ccd8
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-07-16 23:19:22 +0200

    Bla

---
+++ src/PositivNumbersSum/ListTwo.java
@@ -0,0 +1,11 @@
+package PositivNumbersSum;
+
+import java.util.Scanner;
+
+public class ListTwo {
+    public static Double scannerFromUser (){
+        Scanner fromUserKeyboard = new Scanner(System.in);
+        System.out.println("Podaj proszę wybraną przez siebię liczbę.");
+        return fromUserKeyboard.nextDouble();
+    }
+}
---
+++ src/PositivNumbersSum/Test.java
@@ -0,0 +1,8 @@
+package PositivNumbersSum;
+
+public class Test {
+    public static void main(String[] args) {
+        ListThree listThree = new ListThree();
+        listThree.loopOne();
+    }
+}
